5d5ef787a4ea03ab51f2e9f42f69f285500cf153
billy3b <avinashroy45678@gmail.com>
2023-03-07 15:08:48 +0530

    added another approach

--- Recursion/125-valid-palindrome.cpp
+++ Recursion/125-valid-palindrome.cpp
@@ -1,3 +1,4 @@
+//Approach1-recursion
 class Solution {
 public:
 
@@ -21,5 +22,34 @@ public:
         int j=0;
         int k=arr.size()-1;
         return solve(arr,j,k);
+    }
+};
+
+
+//Approach2-two pointer
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string modifiedString;
+
+        // Convert to lowercase and remove non-alphanumeric characters
+        for (char c : s) {
+            if (isalnum(c)) {
+                modifiedString += tolower(c);
+            }
+        }
+
+        // Check if the modified string is a palindrome
+        int left = 0;
+        int right = modifiedString.length() - 1;
+        while (left < right) {
+            if (modifiedString[left] != modifiedString[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 };
