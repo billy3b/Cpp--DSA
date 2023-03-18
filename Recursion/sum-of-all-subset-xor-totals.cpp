89d544239c66b7039db8edf608b36461fc2954db
billy3b <avinashroy45678@gmail.com>
2023-03-18 16:22:47 +0530

    added another approach

--- Recursion/sum-of-all-subset-xor-totals.cpp
+++ Recursion/sum-of-all-subset-xor-totals.cpp
@@ -1,3 +1,4 @@
+//Approach 1 bit manipulation
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
@@ -9,3 +10,29 @@ public:
     }
 };
 
+// Approach2 backtracking
+class Solution {
+public:
+vector<int> ans;
+vector<int> ds;
+
+void generateSubs(vector<int> &nums , int index){
+    if(index == nums.size()){
+        int xorAns=0;
+        for(auto &i:ds){
+            xorAns ^= i;
+        }
+        ans.push_back(xorAns);
+        return;
+    }
+    ds.push_back(nums[index]);
+    generateSubs(nums,index+1);
+    ds.pop_back();
+    generateSubs(nums,index+1);
+}
+int subsetXORSum(vector<int>& nums) {
+
+    generateSubs(nums,0);
+    return accumulate(ans.begin(),ans.end(),0);
+}
+};
