48f1ca4e1d3841d9dc4fea76462b523a97e42fc6
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-06-21 23:20:25 +0530

    Create productexceptindex.cpp

---
+++ productexceptindex.cpp
@@ -0,0 +1,33 @@
+
+//Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+//For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6]
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> productexcepti(vector<int>& arr){
+    int n=arr.size();
+    vector<int> output;
+    for(int i=0;i<n;i++){
+        int product = 1;
+        for(int j=0;j<n;j++){
+            if(i==j){ 
+                continue;
+            }
+            product*=arr[j];
+        }
+        output.push_back(product);
+    }
+        
+    return output;
+}
+int main() {
+    vector<int> arr ={10,2,3,5,7};
+    vector<int> output = productexcepti(arr);
+    for(int i=0;i<output.size(); i++){
+        cout<<output[i]<<" ";
+    }
+    return 0;
+}
