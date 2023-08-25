904f6e571df1b0f0362a8fda073b3fa5279e1fb3
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-08-25 21:38:51 +0530

    countingbits

---
+++ bit manipulation/countingbits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int popCount(int x) {
+		int count;
+		for (count = 0; x != 0; ++count) {
+			x &= x - 1;
+		}
+		return count;
+	}
+	vector<int> countBits(int n) {
+		vector<int> ans(n + 1);
+		for (int x = 0; x <= n; ++x) {
+			ans[x] = popCount(x);
+		}
+		return ans;
+	}
+};
