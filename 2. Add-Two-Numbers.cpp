30131ebb722bf2be6f90ed680022661dd7fce617
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-10-02 18:47:00 +0530

    Update 2. Add-Two-Numbers.cpp

--- 2. Add-Two-Numbers.cpp
+++ 2. Add-Two-Numbers.cpp
@@ -31,3 +31,40 @@ public:
         return dummy->next;
     }
 };
+************************************************************************************************************************************************************************************************************************
+//using Recursion
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addList(ListNode* l1, ListNode* l2, int carry){
+        if(!l1 && !l2){
+            if(carry>0){
+                return new ListNode(carry);
+            }
+            return nullptr;
+        }
+            int x= (l1)? l1->val:0;
+            int y=(l2)? l2->val:0;
+            int sum =x+y+carry;
+            carry=sum/10;
+            ListNode* result = new ListNode(sum%10);
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+
+            result->next = addList(l1,l2,carry);
+            return result;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return addList(l1,l2,0);
+    }
+};
