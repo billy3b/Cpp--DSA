08fa5b1e6ecbb355f1b527d354f95a372829565e
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-10-02 23:42:37 +0530

    Update 206. Added iterative approach

--- Recursion/206. Reverse-linked-list.cpp
+++ Recursion/206. Reverse-linked-list.cpp
@@ -23,3 +23,23 @@ public:
     }
 
 };
+********************************************************************************************************************************************************************
+//Iterative
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+            
+        }
+        return prev;
+    }
+};
