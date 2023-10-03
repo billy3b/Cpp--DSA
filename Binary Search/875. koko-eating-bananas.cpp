6996a5ccdfec6297d18ff6843a8c53fc958c8263
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-10-03 20:10:25 +0530

    Create 875. koko-eating-bananas.cpp

---
+++ Binary Search/875. koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+	bool check(vector<int>& piles, int mid, int h){
+		int time =0;
+
+		for(int i=0;i<piles.size();i++){ 
+			time+=ceil( (double)piles[i] / (double) mid);
+		}
+		if(time<=h){
+			return true;
+		} else{
+			return false;
+		}
+
+	}
+
+	int minEatingSpeed(vector<int>& piles, int h) {
+		// Your code goes here
+		int start = 1;
+		int end = *max_element(piles.begin(), piles.end());
+		
+		while(start<end){
+			int mid = start+ (end-start)/2;
+
+			if(check(piles,mid,h) == true){
+				end=mid;
+			} else{
+				start=mid+1;
+			}
+		}
+		return end;
+	}
+};
