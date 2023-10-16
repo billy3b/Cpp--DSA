915e346a1d1e516bfd37f4225676fa97b15e6081
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-10-16 12:39:29 +0530

    Time: 124 ms (90.94%), Space: 110.1 MB (14.28%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        if (nums.size() == 1) {
+            return static_cast<double>(nums[0]);
+        }
+        
+        int start = 0;
+        int end = k;
+        double average = 0.0;
+        
+        // Calculate the average of the first window
+        for (int i = 0; i < k; i++) {
+            average += static_cast<double>(nums[i]) / k;
+        }
+        
+        // Initialize the maximum average
+        double max_average = average;
+        
+        // Slide the window and update the maximum average
+        while (end < nums.size()) {
+            average = average - static_cast<double>(nums[start]) / k;
+            average = average + static_cast<double>(nums[end]) / k;
+            max_average = max(max_average, average);
+            start++;
+            end++;
+        }
+        
+        return max_average;
+    }
+};
