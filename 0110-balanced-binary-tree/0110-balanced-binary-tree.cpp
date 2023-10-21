81cd7a9b63f6cdc83b4e5acf1fd50c90a4d916c2
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-10-21 23:28:32 +0530

    Time: 9 ms (57.31%), Space: 22.3 MB (6.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,28 +10,30 @@
  * };
  */
 class Solution {
-private:
-        int height(TreeNode* root){
-            if(root == NULL){
-                return 0;
-            }
-            int left = height(root->left);
-            int right = height(root->right);
-            return max(left, right)+1;
-        }
+
 public:
-    bool isBalanced(TreeNode* root) {
+
+    pair<bool, int> isBalancedFast(TreeNode* root){
         if(root == NULL){
-            return true;
+            pair<bool, int> p = make_pair(true,0);
+            return p;
         }
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        bool diff = abs( height(root->left)- height(root->right) )<=1;
-        
-        if(left && right && diff){
-            return 1;
+        pair<bool, int> left = isBalancedFast(root->left);
+        pair<bool, int> right = isBalancedFast(root->right);
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        bool diff = abs( left.second- right.second )<=1;
+        pair<bool,int> ans;
+        ans.second = max(left.second, right.second)+1;
+        if(leftAns && rightAns && diff){
+            ans.first = true;
         } else{
-            return false;
+            ans.first = false;
         }
+        return ans;
+    }
+    
+    bool isBalanced(TreeNode* root) {
+        return isBalancedFast(root).first;
     }
 };
